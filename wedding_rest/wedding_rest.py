488cd8dbf246c9abf3fe9eac4a6586539ad58417
francois-travais <francois.travais@eniram.fi>
2014-12-24 11:53:56 +0200

    + errorhandler + password handling

--- wedding_rest/wedding_rest.py
+++ wedding_rest/wedding_rest.py
@@ -1,10 +1,10 @@
 # -*- coding: utf8 -*-
 import datetime
+import ConfigParser
 
 from flask import Flask, jsonify, request, abort
 from pymongo import MongoClient
 from flask_cors import CORS, cross_origin
-import ConfigParser
 import os
 
 configParser = ConfigParser.SafeConfigParser()
@@ -52,6 +52,15 @@ def retrieve_if_exists(req, field, result):
     return result
 
 
+def validate_password(password):
+    result = db.passwords.find_one({"password": password})
+    if result is not None:
+        app.logger.info('Password of user %s found' % result['user'])
+        return True
+    app.logger.warn('Password %s not found' % password)
+    return False
+
+
 @app.route('/v1/accommodations', methods=['GET'])
 @cross_origin()
 def get_accommodations():
@@ -65,7 +74,10 @@ def get_accommodations():
 @app.route('/v1/contact', methods=['POST'])
 @cross_origin()
 def post_contact():
-    if not request.json or 'name' not in request.json or 'message' not in request.json:
+    if not request.json or 'password' not in request.json or not validate_password(request.json['password']):
+        app.logger.warning('Attempt to post contact form with wrong password')
+        return forbidden('wrong password')
+    if 'name' not in request.json or 'message' not in request.json:
         app.logger.warning("Contact: Missing name or message in contact form")
         abort(400)
     app.logger.debug("Contact: access")
@@ -74,7 +86,7 @@ def post_contact():
         'message': request.json['message'],
         'date': datetime.datetime.utcnow()
     }
-    contact = retrieve_if_exists(request, 'email', contact);
+    contact = retrieve_if_exists(request, 'email', contact)
     contacts = db.contacts
     contact_id = contacts.insert(contact)
     result = contacts.find_one({"_id": contact_id})
@@ -84,7 +96,10 @@ def post_contact():
 @app.route('/v1/reply', methods=['POST'])
 @cross_origin()
 def post_reply():
-    if not request.json or 'name' not in request.json or 'adultNb' not in request.json:
+    if not request.json or 'password' not in request.json or not validate_password(request.json['password']):
+        app.logger.warning('Attempt to post reply form with wrong password')
+        return forbidden('wrong password')
+    if 'name' not in request.json or 'adultNb' not in request.json:
         app.logger.warning("Reply: Missing name or adult number")
         abort(400)
     if not isinstance(request.json['adultNb'], (int, long)):
@@ -106,6 +121,31 @@ def post_reply():
     reply_id = replies.insert(reply)
     result = replies.find_one({"_id": reply_id})
     return jsonify({'reply': unmongoised(result)}), 201
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({'result': 'error', 'data': 'bad request'}), 400
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return jsonify({'result': 'error', 'data': 'forbidden'}), 403
+
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({'result': 'error', 'data': 'unauthorized'}), 401
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({'result': 'error', 'data': 'url not found'}), 404
+
+
+@app.errorhandler(500)
+def runtime_error(error):
+    return jsonify({'result': 'error', 'data': 'internal server error'}), 500
 
 
 if __name__ == '__main__':
